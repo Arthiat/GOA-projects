fea4b52c665ab9274f5d2e8d98ab7c662741a671
Arthiat <arthurleywin912@gmail.com>
2025-02-24 20:20:00 +0400

    4

---
+++ day4/hw/hw3.py
@@ -0,0 +1,14 @@
+qula = int(input("migebuli qula: "))
+
+if qula>90:
+    print("A")
+elif 75<qula<90:
+    print("B")
+elif 60<qula:
+    print("C")
+elif 50<qula:
+    print("D")
+elif qula<40:
+    print("E")
+elif qula<30:
+    print("F")
